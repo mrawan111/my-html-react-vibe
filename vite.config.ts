2571361e255ddf86c4e4055a0f31299f7771bf8e
mrawan111 <135566330+mrawan111@users.noreply.github.com>
2025-09-28 17:31:19 +0300

    kk

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
     port: 5000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'my-html-react-vibe-production.up.railway.app',
         changeOrigin: true,
       },
     },
